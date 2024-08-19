925fa21dd8b0f4af51ae8186cceae154e49b6a16
Isaiah-Ang <isaiah.ang@gmail.com>
2024-08-20 00:29:38 +0800

    feat: add montserrat-regular and montserrat-bold for font styling

--- recipe-genie-app/app/_layout.tsx
+++ recipe-genie-app/app/_layout.tsx
@@ -1,9 +1,39 @@
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
+import { View } from "react-native";
+import { configureFonts, MD3LightTheme, PaperProvider, Text, useTheme } from "react-native-paper";
+import { useFonts } from 'expo-font';
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'Montserrat-Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat-Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
+  });
+
+  const baseFont = {
+    fontFamily: 'Montserrat-Regular',
+  } as const;
+
+  const fonts = configureFonts({ config: baseFont });
+
+  const theme = useTheme();
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <ConvexProvider client={convex}>
+      <PaperProvider theme={{ ...theme, fonts }}>
+        <Stack>
+          <Stack.Screen
+            name="(tabs)"
+            options={{
+              headerShown: false
+            }}
+          />
+        </Stack>
+      </PaperProvider>
+    </ConvexProvider>
   );
 }
